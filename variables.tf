19a94f4bfbdc096bbf3edc3aac9e1091dd2cb62c
Micael Vinhas <mvinhas.tirso@gmail.com>
2025-08-26 10:35:23 +0100

    Switch to using tfe_outputs

--- variables.tf
+++ variables.tf
@@ -37,6 +37,17 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
+#TFC variables
+variable "tfe_organization" {
+  type        = string
+  description = "(Required) The name of the organization in which the workspace is located."
+}
+
+variable "tfe_workspace_name" {
+  type        = string
+  description = "(Required) The name of the workspace whose outputs will be retrieved."
+}
+
 variable "public_subnets" {
   type        = list(string)
   description = "(Required) List of subnet IDs for EC2 instance deployments."
@@ -46,5 +57,3 @@ variable "vpc_id" {
   type        = string
   description = "(Required) VPC ID of VPC for application deployment."
 }
-
-
